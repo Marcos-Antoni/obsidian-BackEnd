3716f60fc1b8302fc233953a09e52e58eddfbadb
marcob-dev <marcos7barrera@gmail.com>
2025-01-22 19:23:39 -0600

    Cambiando promp

--- src/prompt_templates.py
+++ src/prompt_templates.py
@@ -5,20 +5,20 @@ Se lo mas fielmente posible al texto que te dan.
 
 Así se verán los separadores de texto:
 
-* Idea: contenido de la idea
-* Idea: contenido de la idea
-* Idea: contenido de la idea
+{{{Idea: contenido de la idea}}}
+{{{Idea: contenido de la idea}}}
+{{{Idea: contenido de la idea}}}
 etc...
 
 asi hasta que todo el texto haya sido separado por ideas.
 
 Ejemplo real:
-* Pandas es una biblioteca de software escrita en el lenguaje de programación Python para la manipulación y análisis de datos.
-* Ofrece estructuras de datos y operaciones para manipular tablas numéricas y series temporales.
-* Es software libre, lanzado bajo la licencia BSD tres-cláusula.
-* El nombre "Pandas" se deriva del término "panel data", utilizado en econometría para referirse a conjuntos de datos que incluyen observaciones sobre múltiples período
-* también es una referencia al juego de palabras con la frase "Python data analysis".
-* Fue iniciado por Wes McKinney mientras trabajaba en AQR Capital desde 2007 hasta 2010.
+{{{Pandas es una biblioteca de software escrita en el lenguaje de programación Python para la manipulación y análisis de datos.}}}
+{{{Ofrece estructuras de datos y operaciones para manipular tablas numéricas y series temporales.}}}
+{{{Es software libre, lanzado bajo la licencia BSD tres-cláusula.}}}
+{{{El nombre "Pandas" se deriva del término "panel data", utilizado en econometría para referirse a conjuntos de datos que incluyen observaciones sobre múltiples período}}}
+{{{también es una referencia al juego de palabras con la frase "Python data analysis".}}}
+{{{Fue iniciado por Wes McKinney mientras trabajaba en AQR Capital desde 2007 hasta 2010.}}}
 """
 
 
